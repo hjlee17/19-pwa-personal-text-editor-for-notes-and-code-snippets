ffc5546cab8562ccebcc9e0b59b8413ae3ebf801
hjlee <heunjong.lee@gmail.com>
2024-01-30 22:51:18 -0500

    updated entrypoints

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,10 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO-DONE: Add and configure workbox plugins for a service worker and manifest file.
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
-// TODO: Add CSS loaders and babel to webpack.
+// TODO-DONE: Add CSS loaders and babel to webpack.
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
@@ -14,7 +14,11 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
+
     },
     output: {
       filename: '[name].bundle.js',
@@ -23,7 +27,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'JATE',
       }),
       new MiniCssExtractPlugin(),
       new WorkboxPlugin.GenerateSW(),
